61f11c71fcf0573bb326d14be0431d555ae9b593
Passant-Abdelgalil <basant.a.abdelgalil@gmail.com>
2022-08-05 22:59:30 +0200

    feat: :sparkles: responsive features section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -3,7 +3,7 @@ import styles from "./Feature.module.scss";
 
 const Feature = (props) => {
   return (
-    <div className={`${props.className} feature`}>
+    <div className={`${styles['feature']} ${props.className}`}>
       <h2 className={styles["feature__heading"]}>{props.title}</h2>
       <p className={styles["feature__paragraph"]}>{props.text}</p>
     </div>
--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Feature } from "../../components";
 import styles from "./Features.module.scss";
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <section className={styles["gpt3__features"]}>
+      <div className={styles["gpt3__features-heading"]}>
+        <h2 className={`gradient__text ${styles["gpt3__features-heading_h"]}`}>
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen.
+        </h2>
+        <p className={styles["gpt3__features-heading_p"]}>
+          Request Early Access to Get Started
+        </p>
+      </div>
+      <div className={styles["gpt3__features-content"]}>
+        <Feature
+          className={styles["gpt3__features-content_item"]}
+          title="Improving end distrusts instantly"
+          text="From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
+        />
+        <Feature
+          className={styles["gpt3__features-content_item"]}
+          title="Become the tended active"
+          text="Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to."
+        />
+        <Feature
+          className={styles["gpt3__features-content_item"]}
+          title="Message or am nothing"
+          text="Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
+        />
+        <Feature
+          className={styles["gpt3__features-content_item"]}
+          title="Really boy law county"
+          text="Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush."
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Features;
