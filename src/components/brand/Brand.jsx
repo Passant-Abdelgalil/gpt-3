9d9a6cf174f50426451974f9fcf28e6f7479d8a8
Passant-Abdelgalil <basant.a.abdelgalil@gmail.com>
2022-08-05 17:21:32 +0200

    feat: :sparkles: responsive brand section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styles from "./Brand.module.scss";
+/* Sponsers image => should be from API */
+import google from "../../assets/google.png";
+import shopify from "../../assets/shopify.png";
+import atlassian from "../../assets/atlassian.png";
+import dropbox from "../../assets/dropbox.png";
+import slack from "../../assets/slack.png";
 
 const Brand = () => {
-  return <div className="test">Brand</div>;
+  const sponsers = /* should be from API */ [
+    { title: "google", img: google },
+    { title: "slack", img: slack },
+    { title: "atlassian", img: atlassian },
+    { title: "dropbox", img: dropbox },
+    { title: "shopify", img: shopify },
+  ];
+  return (
+    <div className={styles["sponsers"]}>
+      {sponsers.map((sponser) => (
+        <img
+          className={styles["sponsers_img"]}
+          src={sponser.img}
+          alt={sponser.title}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Brand;
