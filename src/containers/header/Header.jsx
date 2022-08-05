bde328b6652fa0a555779395355bec80a3559957
Passant-Abdelgalil <basant.a.abdelgalil@gmail.com>
2022-08-05 17:21:15 +0200

    feat: :sparkles: responsive header section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,7 +1,72 @@
 import React from "react";
 import styles from "./Header.module.scss";
+
+import headerImage from "../../assets/Header Illustration.png";
+
+import defaultProfile from "../../assets/profile.jpg";
+
 const Header = () => {
-  return <div>Header</div>;
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const requestingPeople = /* should be from API*/ [
+    defaultProfile,
+    defaultProfile,
+    defaultProfile,
+    defaultProfile,
+    defaultProfile,
+    defaultProfile,
+    defaultProfile,
+    defaultProfile,
+    defaultProfile,
+    defaultProfile,
+  ];
+  const requestsNumber = /* should be requestingPeople.length*/ 1600;
+
+  return (
+    <section className={styles["header-section"]}>
+      <div className={styles["header"]}>
+        <div className={styles["header__content"]}>
+          <h1
+            className={`gradient__text ${styles["header__content-headline"]}`}
+          >
+            Let's Build Something amazing with GPT-3 OpenAI
+          </h1>
+          <p className={styles["header__content-paragraph"]}>
+            Yet bed any for travelling assistance indulgence unpleasing. Not
+            thoughts all exercise blessing. Indulgence way everything joy
+            alteration boisterous the attachment. Party we years to order allow
+            asked of.
+          </p>
+          <form
+            className={styles["header__content-getStarted"]}
+            onSubmit={submitFormHandler}
+          >
+            <input type="email" placeholder="Your Email Address" />
+            <button type="submit">Get Started</button>
+          </form>
+          <div className={styles["header__content-requests"]}>
+            <div className={styles["header__content-requests_images"]}>
+              {requestingPeople.slice(0, 6).map((item) => (
+                <img src={item} alt="profile" />
+              ))}
+              <p className={styles["header__content-requests_images-more"]}>
+                1.6k+
+              </p>
+            </div>
+            <p className={styles["header__content-requests_paragraph"]}>
+              {requestsNumber.toLocaleString()} people requested access a visit
+              in last 24 hours
+            </p>
+          </div>
+        </div>
+        <div className={styles["header__image"]}>
+          <img src={headerImage} alt="header illustration" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Header;
