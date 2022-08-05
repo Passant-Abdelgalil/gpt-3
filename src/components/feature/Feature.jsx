0042e3bdaa7b65045167f22acd346b08be5e7c69
Passant-Abdelgalil <basant.a.abdelgalil@gmail.com>
2022-08-05 20:33:02 +0200

    feat: :sparkles: What is GPT-3 section with responsiveness

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import styles from "./Feature.module.scss";
 
-const Feature = () => {
+const Feature = (props) => {
   return (
-    <div>Feature</div>
-  )
-}
+    <div className={`${props.className} feature`}>
+      <h2 className={styles["feature__heading"]}>{props.title}</h2>
+      <p className={styles["feature__paragraph"]}>{props.text}</p>
+    </div>
+  );
+};
 
-export default Feature
+export default Feature;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,7 +1,55 @@
 import React from "react";
 import styles from "./WhatGPT3.module.scss";
+import { Feature } from "../../components";
+
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <section id="wgpt3" className={styles["wgpt3"]}>
+      <div className={styles["wgpt3-wrapper"]}>
+        <Feature
+          className={styles["wgpt3__intro"]}
+          title="What is GPT-3"
+          text="We so opinion friends me message as delight. Whole front do of plate
+            heard oh ought. His defective nor convinced residence own.
+            Connection has put impossible own apartments boisterous. At jointure
+            ladyship an insisted so humanity he. Friendly bachelor entrance to
+            on by."
+        />
+        <div className={styles["wgpt3__features"]}>
+          <div className={styles["wgpt3__features-header"]}>
+            <h2
+              className={`gradient__text ${styles["wgpt3__features-header-h"]}`}
+            >
+              The possibilities are beyond your imagination
+            </h2>
+            <a href="/" className={styles["wgpt3__features-header--explore"]}>
+              Explore The Library
+            </a>
+          </div>
+          <div className={styles["wgpt3__features-content"]}>
+            <Feature
+              className={styles["wgpt3__features-content_item"]}
+              title="Chatbots"
+              text="We so opinion friends me message as delight. Whole front do of
+                plate heard oh ought."
+            />
+            <Feature
+              className={styles["wgpt3__features-content_item"]}
+              title="Knowledgebase"
+              text="At jointure ladyship an insisted so humanity he. Friendly
+                bachelor entrance to on by. As put impossible own apartments"
+            />
+            <Feature
+              className={styles["wgpt3__features-content_item"]}
+              title="Education"
+              text="At jointure ladyship an insisted so humanity he. Friendly
+                bachelor entrance to on by. As put impossible own apartments"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default WhatGPT3;
