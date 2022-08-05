5e81d9e49b8b6f263f326424f6c851a39709c94d
Passant-Abdelgalil <basant.a.abdelgalil@gmail.com>
2022-08-05 17:20:07 +0200

    feat: :sparkles: responsive navigation bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, {Fragment, useState} from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import styles from "./Navbar.module.scss";
+import logo from "../../assets/logo.svg";
+
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-export default Navbar
+  const Menu = () => (
+    <Fragment>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+    </Fragment>
+    );
+  
+  return (
+    <navbar className={styles["gpt3__navbar"]}>
+      <div className={styles["gpt3__navbar-links"]}>
+        <div className={styles["gpt3__navbar-links_logo"]}>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className={styles['gpt3__navbar-links_container']}>
+          <Menu/>
+        </div>
+        <div className={styles['gpt3__navbar-sign']}>
+          <p className={styles['gpt3__navbar-sign_in']}>Sign in</p>
+          <button type="button" className={styles['gpt3__navbar-sign_up']}>Sign up</button>
+        </div>
+        <div className={styles['gpt3__navbar-menu']}>
+        {toggleMenu 
+        ? <RiCloseLine color="#fff" size={27} onClick={()=>setToggleMenu(false)}/>
+        : <RiMenu3Line color="#fff" size={27} onClick={()=>setToggleMenu(true)}/>
+        }
+        {toggleMenu && (
+          <div className={`scale-up-center ${styles['gpt3__navbar-menu_container']}`}>
+            <div className={styles['gpt3__navbar-menu_container-links']}>
+              <Menu/>
+              <div className={styles['gpt3__navbar-menu_container-links-sign']}>
+                <p className={styles['gpt3__navbar-menu_container-links-sign_in']}>Sign in</p>
+                <button type="button" className={styles['gpt3__navbar-menu_container-links-sign_up']}>Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+        </div>
+      </div>
+    </navbar>
+  );
+};
+
+export default Navbar;
