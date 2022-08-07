266037def43410a4ecba2a6b7b56060b06995191
Passant-Abdelgalil <basant.a.abdelgalil@gmail.com>
2022-08-07 18:55:45 +0200

    feat: :sparkles: responsive blogs section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import styles from "./Article.module.scss";
-const Article = () => {
-  return <div className={styles["test"]}>Article</div>;
+const Article = (props) => {
+  return (
+    <div className={`${props.className} ${styles["article"]}`}>
+      <div className={styles["article__image"]}>
+        <img src={props.img} alt="article" />
+      </div>
+      <div className={styles["article__content"]}>
+        <p className={styles["article__content_date"]}>{props.date}</p>
+        <h3 className={styles["article__content_title"]}>{props.title}</h3>
+        <p className={styles["article__content--more"]}>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from "react";
 import styles from "./Blog.module.scss";
+import { Article } from "../../components";
 
+import img1 from "../../assets/Rectangle 22.png";
+import img2 from "../../assets/Rectangle 22-1.png";
+import img3 from "../../assets/Rectangle 22-2.png";
+import img4 from "../../assets/Rectangle 22-3.png";
+import img5 from "../../assets/Rectangle 22-4.png";
 
 const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
-}
+  const articles = [
+    {
+      img: img1,
+      title: "GPT-3 and Open AI is the future. Let us explore how it is?",
+      data: "Sep 26, 2021",
+      link: "",
+    },
+    {
+      img: img2,
+      title: "GPT-3 and Open AI is the future. Let us explore how it is?",
+      data: "Sep 26, 2021",
+      link: "",
+    },
+    {
+      img: img3,
+      title: "GPT-3 and Open AI is the future. Let us explore how it is?",
+      data: "Sep 26, 2021",
+      link: "",
+    },
+    {
+      img: img4,
+      title: "GPT-3 and Open AI is the future. Let us explore how it is?",
+      data: "Sep 26, 2021",
+      link: "",
+    },
+    {
+      img: img5,
+      title: "GPT-3 and Open AI is the future. Let us explore how it is?",
+      data: "Sep 26, 2021",
+      link: "",
+    },
+  ];
 
-export default Blog
+  return (
+    <section className={styles["gpt3__blog"]}>
+      <h2 className={`gradient__text ${styles["gpt3__blog-headline"]}`}>
+        A lot is happening,
+      </h2>
+      <h2 className={`gradient__text ${styles["gpt3__blog-headline"]}`}>
+        We are blogging about it.
+      </h2>
+      <div className={styles["gpt3__blog-articles"]}>
+        {articles.map((article) => (
+          <Article
+            className={styles["gpt3__blog-articles_item"]}
+            img={article.img}
+            title={article.title}
+            date={article.data}
+            link={article.link}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Blog;
